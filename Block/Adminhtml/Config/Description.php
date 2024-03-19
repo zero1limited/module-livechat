adb27673aa4f1616fdc9b5e57a9bd6e27556efd9
Arron Moss <arron.moss@zero1.co.uk>
2024-03-19 09:16:14 +0000

    Update Description.php

--- Block/Adminhtml/Config/Description.php
+++ Block/Adminhtml/Config/Description.php
@@ -12,8 +12,7 @@ class Description extends \Magento\Config\Block\System\Config\Form\Fieldset
      */
     public function render(\Magento\Framework\Data\Form\Element\AbstractElement $element)
     {
-        return '<img src="https://www.zero1.co.uk/wp-content/uploads/2023/01/cropped-new-zero-1-logo-2.png" width="80" height="38">
-        <h2>ZERO-1 Ecommerce</h2>
-        <p>Sign up for LiveChat <a href="https://www.livechat.com/"> here</a>.</p>';
+        return '<h2>ZERO-1 Ecommerce</h2>
+        <p>Sign up for a <a href="https://bit.ly/hyva-livechat">LiveChat account with a free 30 day trial here</a>.</p>';
     }
 }
